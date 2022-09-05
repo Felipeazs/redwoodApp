b6443ae1079c064b8581e266a7ae053d2274cbc6
felipeazs <felipeazs@gmail.com>
2022-09-04 20:56:46 -0400

    Create Layouts... yarn rw g layout blog. Import BlogLayout in the Routes.js page.

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,24 +1,10 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
     return (
         <>
             <MetaTags title="About" description="About page" />
-
-            <header>
-                <h1>Redwood Blog</h1>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to={routes.about()}>About</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <main>Home</main>
-            <p>Redwood link examples</p>
-            <Link to={routes.home()}>Return Home</Link>
+            <main>About</main>
         </>
     )
 }
