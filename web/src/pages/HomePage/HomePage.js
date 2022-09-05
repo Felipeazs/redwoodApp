47d02bceb43249ed430418675251ddfd751ebc47
felipeazs <felipeazs@gmail.com>
2022-09-04 21:31:59 -0400

    Creating cell for automatic linked graphql results to components, including loading an error handling. yarn rw g cell Articles. Generates component ArticlesCell.

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,15 @@
 import { MetaTags } from '@redwoodjs/web'
 
+import ArticlesCell from '../../components/ArticlesCell'
+
 const HomePage = () => {
     return (
         <>
             <MetaTags title="Home" description="Home page" />
 
-            <main>Home</main>
+            <>
+                <ArticlesCell />
+            </>
         </>
     )
 }
